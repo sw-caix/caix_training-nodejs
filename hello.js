6f186545c85bf63dcc92d45e20b0c713ef22f918
caixiaoyi <caix@solutionware.jp>
2026-05-08 13:31:00 +0900

    yargsモジュールメソッド

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+import yargs from 'yargs/yargs';
 import { check } from './function.js';
 // const { check }= require('./function.js');
 
@@ -6,3 +7,11 @@ console.log("Hello World !")
 
 // メインのファイルで別のファイルのfunctionを実行する
 check(process.argv[2]);
+
+// yargsモジュールのメソッドを実行する
+const myArgs = "--name 蔡 --age 0";
+// parseメソッド実行 
+const argv = yargs().parse(myArgs);
+// 結果を表示する
+console.log("名前:", argv.name); // 蔡 
+console.log("年齢:", argv.age);  // 0
